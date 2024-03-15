f12463849e1ebd44571db9800be6e1071eda6cac
citlallivillarreal <citlallivillarreal@outlook.com>
2024-03-15 12:25:43 -0700

    Updated app and index

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import ParticlesBg from 'particles-bg';
+
 
 function App() {
   return (
     <div className="App">
+      <ParticlesBg className='particles' type="cobweb" bg={true} num={350} color="#ffffff"/>
       <Navigation />
       <Logo />
-{/* {      
+      <Rank />
       <ImageLinkForm />
+{/* {      
       <FaceRecognition />} */}
     </div>
   );
