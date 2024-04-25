64949b6a02723475d74815da08956b88a9fab9c5
citlallivillarreal <citlallivillarreal@outlook.com>
2024-04-25 11:40:14 -0700

    fixed register route bug

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -34,12 +34,11 @@ class Register extends React.Component {
         })
             .then(response => response.json())
             .then(user => {
-                if (user) {
+                if (user.id) {
                     this.props.loadUser(user);
                     this.props.onRouteChange('home');
                 }
             })
-        this.props.onRouteChange('home');
     }
 
     render() {
